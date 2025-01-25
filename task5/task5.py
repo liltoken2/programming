736b56761cf8ebb21325af9002f80d7b08b08850
Андрей Боев <a.boev.it@gmail.com>
2025-01-25 16:55:31 +0300

    Оптимизация task5

--- task5/task5.py
+++ task5/task5.py
@@ -1,70 +1,48 @@
 import struct
 
-def infr(x):
-    int_part = ''
-    frac_part = ''
-    fl = False
-    for i in x:
-        if i == '.':
-            fl = True 
-            continue
-        if not(fl):
-            int_part += i
-        if fl :
-            frac_part += i
-    return int_part , frac_part
+def hex_to_oct(x):
+    is_negative = x.startswith('-')
+    if is_negative:
+        x = x[1:]
+    parts = x.split('.')
 
+    integer = oct(int(parts[0], 16))[2:]
+    frac = ''
+    if '.' in x:
+        fractional = parts[1]
+        dec_frac = 0
+        for i, digit in enumerate(fractional):
+            dec_frac += int(digit, 16) * (16 ** -(i + 1))
+        frac = ''
+        while dec_frac > 0 and len(frac) < 10:
+            dec_frac *= 8
+            digit = int(dec_frac)
+            frac += str(digit)
+            dec_frac -= digit
 
-def convert_to_oct(x,func):
-    int_part, frac_part = func(x)
-    x = 0
-    for i in range(1,len(frac_part)+1):
-        x += (int(frac_part[i-1],16))*16**(-i)
-    x = str(x)[1:]
-    x = str(int(int_part,16)) + x
-
-    int_part,frac_part = func(x)
-    x = '.'
-    frac_part = float('0.'+frac_part)
-    while True :
-        frac_part = frac_part * 8
-        if frac_part == int(frac_part):
-            x += str(int(frac_part))
-            break
-        else:
-            x += str(frac_part)[:1]
-            frac_part = frac_part - int(str(frac_part)[:1])    
-    x = oct(int(int_part))[2:] + x
-
-    return x
+    return f'{"-" if is_negative else ""}{integer}.{frac}'
 
 point_counter = 0
 word_quantity =[]
-with open('in.txt','r') as inf, open('out.txt','w') as out:
+with open('in.txt','r') as input, open('out.txt','w') as out, open('out1.bin','wb+') as out1:
     word_counter = 0
-    for line in inf :
-        line = line.strip().split(" ")
-
-        for el in line :
+    for line in input:
+        words = line.strip().split()
+        for word in words:
             num = ''
-            try:
-                num = convert_to_oct(el,infr)
-                line[line.index(el)] = num
-            except Exception:
-                if el == '.' :
-                    point_counter += 1
-                    word_quantity.append(word_counter)
-                    word_counter = 0
-                else :
-                    word_counter += 1
-        for i in range(len(line)) :
-            if i == len(line)-1:
-                out.write('{}\n'.format(line[i]))
+            if all(c in '0123456789abcdef.' for c in word.lower()) and word.count('.') <= 1 and word != '.':
+                num = hex_to_oct(word)
+                words[words.index(word)] = num
             else:
-                out.write('{} '.format(line[i]))
-with open('out1.bin','wb+') as out1:
-    packed1 = struct.pack('i',point_counter)
-    out1.write(packed1)
+                if word[-1] in '.!?':
+                    point_counter += 1
+                    word_quantity.append(word_counter + 1)
+                    word_counter = 0
+                else:
+                    word_counter += 1
+        out.write(f"{' '.join(words)}\n")
+    packed = struct.pack('i',point_counter)
+    out1.write(packed)
     for el in word_quantity:
         packed2 = struct.pack('i',el)
         out1.write(packed2)
